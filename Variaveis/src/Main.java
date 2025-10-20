82e3e39f52abf5252825ae20fb93ca54198726bb
Paulo Filho <paulohacf1@hotmail.com>
2025-10-20 07:26:10 -0300

    aulas inicias batismo de java

---
+++ Variaveis/src/Main.java
@@ -0,0 +1,10 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //Variaveis: é algo que guarda outra coisa; pega espaço na memória para isso;
+        //Linguagem fortemente tipada: precisa declarar antes o tipo da variavel;
+        //Evita problemas de tipagem
+
+    }
+}
